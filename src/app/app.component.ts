9af2172cef0c864f87844019c07568448bda81f1
TheRohitGunjal <you@example.com>
2024-02-15 12:31:05 +0530

    hello

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+title='components-Item';
+desc='components-Item';
+img1='https://www.trocals.com/wp-content/uploads/2023/06/Untitled-design-10-860x450.png';
+img2='https://www.trocals.com/wp-content/uploads/2023/06/Untitled-design-11-1-860x450.png';
+img3='https://www.trocals.com/wp-content/uploads/2023/06/Untitled-design-12-1-860x450.png.webp';
+img4='https://www.trocals.com/wp-content/uploads/2023/06/Untitled-design-13-1-860x450.png.webp';
+img5='https://www.trocals.com/wp-content/uploads/2023/06/Untitled-design-14-860x450.png.webp';
+img6='https://www.trocals.com/wp-content/uploads/2023/06/Untitled-design-15-1-860x450.png.webp';
+img7='https://www.trocals.com/wp-content/uploads/2023/06/Untitled-design-16-2-1024x536.png.webp';
+img8='https://www.trocals.com/wp-content/uploads/2023/06/Masala-Chai-1.jpg';
+img9='https://www.trocals.com/wp-content/uploads/2023/06/Untitled-design-17-860x450.png.webp';
+img10='https://www.trocals.com/wp-content/uploads/2023/06/Untitled-design-18-1-1024x536.png.webp';
+
+
+}
